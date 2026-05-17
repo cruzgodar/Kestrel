53e56e46822bef55e65c00c6b1a0d0e673d15e2f
Cruz Godar <cruz.godar@gmail.com>
2026-05-16 21:30:20 -0400

    Initial Commit

---
+++ Kestrel/KestrelApp.swift
@@ -0,0 +1,17 @@
+//
+//  KestrelApp.swift
+//  Kestrel
+//
+//  Created by Cruz Godar on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct KestrelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
