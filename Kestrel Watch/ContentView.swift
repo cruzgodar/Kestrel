2ac96cbcc4524b200deb44e08fc2befe52aa5ca0
Cruz Godar <cruz.godar@gmail.com>
2026-05-22 13:42:11 -0400

    Got a skeleton Watch app working

---
+++ Kestrel Watch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kestrel Watch Watch App
+//
+//  Created by Cruz Godar on 5/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kestrel Watch/Kestrel_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kestrel_WatchApp.swift
+//  Kestrel Watch Watch App
+//
+//  Created by Cruz Godar on 5/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Kestrel_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
